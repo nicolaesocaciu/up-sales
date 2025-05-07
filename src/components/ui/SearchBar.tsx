9bc5650bad5d7d1d20cdbb59bba9ba67337a50ed
Nicolae Socaciu <103567375+nicolaesocaciu@users.noreply.github.com>
2025-05-07 23:22:26 +0300

    search ajustments

--- src/components/ui/SearchBar.tsx
+++ src/components/ui/SearchBar.tsx
@@ -8,13 +8,13 @@ export const SearchBar = () => {
       <div className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400">
         <Search className="h-4 w-4 text-gray-400" />
       </div>
-      <div className="absolute right-3 top-1/2 transform -translate-y-1/2 flex items-center gap-2 text-gray-400">
+      <div className="absolute right-3 top-1/2 transform -translate-y-1/2 flex items-center gap-1 text-gray-400">
         <kbd className="px-1.5 py-0.5 text-xs rounded bg-[#252626] text-white">⌘</kbd>
         <kbd className="px-1.5 py-0.5 text-xs rounded bg-[#252626] text-white">K</kbd>
       </div>
       <Input
         style={{ backgroundColor: "#494A4A" }}
-        className="pl-10 pr-20 border-0 bg-opacity-70 placeholder-gray-400 text-white focus:ring-primary"
+        className="pl-10 pr-20 border-0 bg-[#494A4A] placeholder-[#FFFFFF] text-white focus:ring-primary"
         placeholder="Search..."
       />
     </div>
