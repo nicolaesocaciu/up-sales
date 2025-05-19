f1bd8c8fac466c54d784472e515cef1337aa1556
Nicolae Socaciu <103567375+nicolaesocaciu@users.noreply.github.com>
2025-05-19 21:32:55 +0300

    boxes

--- src/components/onboarding/ui/ModuleCard.tsx
+++ src/components/onboarding/ui/ModuleCard.tsx
@@ -88,7 +88,7 @@ export const ModuleCard = ({
       className={`
         rounded-[16px] p-6 cursor-pointer transition-colors flex flex-col items-center justify-center
         ${selected 
-          ? 'box-shadow-[0px_5px_10px_0px_rgba(27,56,76,0.1)] bg-[#E7F2F9] shadow-[inset_0_0_0_4px_#1482CC]' 
+          ? 'bg-[#E7F2F9] shadow-[inset_0_0_0_4px_#1482CC]' 
           : 'border border-gray-200 bg-white hover:border-[#1482CC] shadow-[0px_5px_10px_0px_rgba(27,56,76,0.1)]'}
       `}
       onClick={onClick}
