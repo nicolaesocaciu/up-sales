51a89203eed1018921a922fa234aad4273b0e6f7
Nicolae Socaciu <103567375+nicolaesocaciu@users.noreply.github.com>
2025-05-19 23:21:16 +0300

    impor boxes

--- src/components/onboarding/ui/ServiceCard.tsx
+++ src/components/onboarding/ui/ServiceCard.tsx
@@ -20,28 +20,22 @@ export const ServiceCard = ({ title, selected, onClick, iconUrl }: ServiceCardPr
       `}
       onClick={onClick}
     >
-      {/* Inset border effect */}
-      {/* <div className={`
-        absolute inset-0 rounded-lg border-2 
-        ${selected ? 'border-[#116FAE]' : 'border-gray-200'}
-      `}></div> */}
-      
-      <div className="flex-1 flex items-center justify-center mb-4 z-10">
-        {selected ? (
-          <div className="bg-[#116FAE] rounded-full w-12 h-12 flex items-center justify-center">
-            <Check className="text-white h-5 w-5" />
-          </div>
-        ) : iconUrl ? (
-          <div className="w-12 h-12 flex items-center justify-center">
-            <img src={iconUrl} alt={`${title} logo`} className="max-w-full max-h-full" />
-          </div>
-        ) : (
-          <div className="bg-gray-100 w-12 h-12 rounded-full"></div>
-        )}
-      </div>
-      <div className="text-center z-10">
-        <h3 className="font-medium">{title}</h3>
-      </div>
+    <div className="flex-1 flex items-center justify-center mb-4">
+      {selected ? (
+        <div className="bg-[#116FAE] rounded-full w-12 h-12 flex items-center justify-center">
+          <Check className="text-white h-5 w-5" />
+        </div>
+      ) : iconUrl ? (
+        <div className="w-20 h-20 flex items-center justify-center">
+          <img src={iconUrl} alt={`${title} logo`} className="max-w-full max-h-full" />
+        </div>
+      ) : (
+        <div className="bg-gray-100 w-12 h-12 rounded-full"></div>
+      )}
     </div>
+    <div className="text-center">
+      <h3 className={`${selected ? 'font-bold' : 'font-normal'}`}>{title}</h3>
+    </div>
+  </div>
   );
 };
